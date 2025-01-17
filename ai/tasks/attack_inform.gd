ca00ca358e46558982d64b22c903534e718ae039
Zakharii Furmanets <zfurmanec@gmail.com>
2025-01-17 17:29:30 +0100

    kinda finish beast tameress bt

--- ai/tasks/attack_inform.gd
+++ ai/tasks/attack_inform.gd
@@ -1,10 +1,16 @@
-# This task is meant to inform the player of the AI attack.
+# This task is meant to inform the player of the AdI attack.
 # It's being called before the delay before the attack.
 
-extends BaseAction
+extends BTDelay
+class_name AttackInformDelay
 
-func _tick(delta: float) -> Status:
+# @export var seconds: float
+@export var inform_text: String
+
+var controller: Controller
+
+func _enter() -> void:
+	controller = scene_root
 	if !controller:
-		return FAILURE
-	controller.attack_notify()
-	return SUCCESS
+		"Attack Inform ain't gonna work w/o controller"
+	controller.attack_notify(inform_text, seconds)
