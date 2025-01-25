4df99bcef2e17c4619144f6e786e2d507d6c30e0
Zakharii Furmanets <zfurmanec@gmail.com>
2025-01-26 00:52:17 +0100

    logic for tiger and lion

--- ai/tasks/tiger/switch_place.gd
+++ ai/tasks/tiger/switch_place.gd
@@ -1,17 +1,27 @@
 extends BaseAction
 
-var already_switched = false
-var distance_x = 300  # Дистанція, на яку актор повинен зникати за межі екрана
-var speed = 100  # Швидкість переміщення актора
+@export var speed = 400  # Швидкість переміщення актора
+@export var distance_x = 480  # Дистанція, на яку актор повинен зникати за межі екрана
+
 var target_position = Vector2()  # Цільова позиція актора
 var moving_out = true  # Прапорець для контролю напрямку руху
-var anim_tree: AnimationTree 
+var anim_tree: AnimationTree
 
 func _enter() -> void:
 	super()
 	unique_actor = controller.tiger
 	if unique_actor:
-		target_position = unique_actor.global_position + Vector2(distance_x, 0) * (1 if !already_switched else -1)
+		# Визначення дальньої позиції (-2300 або 2300) в залежності від поточної позиції актора
+		var far_position: float = 2000
+		far_position += distance_x
+		if unique_actor.global_position.x < 0:
+			far_position = -far_position
+
+		# Додаємо відстань для виходу за екран
+		target_position = Vector2(far_position, unique_actor.global_position.y)
+		var dir_to_center: Vector2 = unique_actor.global_position.direction_to(Vector2.UP)
+		unique_actor.adjust_scale_for_direction(dir_to_center)
+
 	anim_tree = state_machine.animation_tree
 	state_machine.switch_state("run")
 
@@ -20,26 +30,37 @@ func _tick(delta: float) -> Status:
 		return FAILURE
 
 	# Переміщення до цільової позиції
-	var direction = (target_position - unique_actor.global_position).normalized()
-	unique_actor.global_position += direction * speed * delta
+	var direction = unique_actor.global_position.direction_to(target_position)
+	unique_actor.velocity = direction * speed
 
-	# Перевірка, чи досягнуто цільову позицію
-	if unique_actor.global_position.distance_to(target_position) < 1:
+	# Перевірка, чи досягнуто цільову позицію (вихід за екран)
+	if unique_actor.global_position.distance_to(target_position) < 100.0:
 		if moving_out:
-			# Зміна напрямку для повернення з іншого боку
-			already_switched = !already_switched
-			unique_actor.adjust_scale_for_direction(Vector2(1, 0) if already_switched else Vector2(-1, 0))
-			unique_actor.global_position.x = -unique_actor.global_position.x  # Перемістити на протилежний бік
-			target_position = unique_actor.global_position + Vector2(distance_x, 0) * (1 if already_switched else -1)
+			# Встановлюємо нову позицію для повернення на екран
+			unique_actor.global_position.x = -target_position.x  # Перемістити на протилежний бік
+			unique_actor.adjust_scale_for_direction(Vector2(1, 0) if target_position.x > 0 else Vector2(-1, 0))
+
+			# Нова цільова позиція на екрані
+			target_position.x = unique_actor.global_position.x + sign(target_position.x) * distance_x
 			moving_out = false
+
+			# Оновлення blend_position для руху задом
+			anim_tree.set("parameters/MainStateMachine/RUN/blend_position", -1)
 		else:
 			# Повернення завершено
 			moving_out = true
+			# Оновлення blend_position для руху передом
+			anim_tree.set("parameters/MainStateMachine/RUN/blend_position", 1)
+
 			return SUCCESS
 			
+
+	# Оновлення blend_position для анімації під час руху
 	if moving_out:
 		anim_tree.set("parameters/MainStateMachine/RUN/blend_position", -1)
 	else:
 		anim_tree.set("parameters/MainStateMachine/RUN/blend_position", 1)
+		
+	unique_actor.move_and_slide()
 
 	return RUNNING
--- state machines/Tiger/tiger_hsm.gd
+++ state machines/Tiger/tiger_hsm.gd
@@ -8,3 +8,8 @@ func create_state_conditions():
 	state_conditions["idle"] =  path_to_anim_parameters + "idle"
 	state_conditions["death"] =  path_to_anim_parameters + "death"
 	state_conditions["run"] =  path_to_anim_parameters + "run"
+	state_conditions["jump"] =  path_to_anim_parameters + "jump"
+	state_conditions["fly_up"] =  path_to_anim_parameters + "fly_up"
+	state_conditions["fly_down"] =  path_to_anim_parameters + "fly_down"
+	state_conditions["fly_catch"] =  path_to_anim_parameters + "fly_catch"
+	state_conditions["fly_land"] =  path_to_anim_parameters + "fly_land"
