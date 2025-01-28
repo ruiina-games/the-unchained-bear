6f1eb2079c7c45f6d6c81eadc0dd4a99b5a49d27
Zakharii Furmanets <zfurmanec@gmail.com>
2025-01-28 17:49:44 +0100

    kurva kod zabuv

--- controllers/ai_controllers/beast tameress/beast_tameress_controller.gd
+++ controllers/ai_controllers/beast tameress/beast_tameress_controller.gd
@@ -18,7 +18,7 @@ func setup_monkey():
 			return
 		var throw_direction: Vector2 = actor.global_position.direction_to(target.global_position)
 		actor.adjust_scale_for_direction(throw_direction)
-		monkey.object.damage = actor.character_stats.fighting_style.get_damage()
+		monkey.object.damage_index = actor.character_stats.fighting_style.combo_count
 		monkey.throw_object(throw_direction.normalized())
 	)
 
