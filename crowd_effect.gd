714cc8481bf1ac6c8f842856733986b4ea70f441
Andrian Furmenets <andrianfurmanec@gmail.com>
2025-01-30 16:34:02 +0200

    levels 1 and 3 are done

---
+++ crowd_effect.gd
@@ -0,0 +1,18 @@
+extends Control
+class_name CrowdEffect
+
+@export var player_controller: PlayerController
+@export var crowd_anim_player: AnimationPlayer
+
+func _ready():
+	GlobalSignals.hurt_triggered.connect(func(causer): if causer and causer.name == "BearNew": crowd_cheer())
+	
+func crowd_cheer():
+	if crowd_anim_player.current_animation == "crowd_cheer":
+		return
+	
+	var random_number = randi_range(0, 10)
+	if random_number > 8:
+		crowd_anim_player.play("crowd_cheer")
+		crowd_anim_player.animation_finished.connect(func(anim_name): if anim_name == "crowd_cheer": crowd_anim_player.play("crowd_idle"))
+		
